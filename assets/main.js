bdbcdf4691c2d038442b8353485955dd4d0292f9
chry902 <tchry902@gmail.com>
2021-10-15 15:28:32 +0200

    avanzamento

--- assets/main.js
+++ assets/main.js
@@ -21,5 +21,15 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
-let ciao = "ciao";
-console.log(ciao);
+
+/*
+-cliccare sui bottoni e far scorrere l'immagini
+-fare apparire l'immagine scelta dalle frecce
+*/
+
+
+
+for (let i = 0; i < items.length; i++) {
+    const jumbo_img = items[i];
+    console.log(items);
+}
